4263b4f6be4fa967e349002792be60021f32006f
kiki <dakikifr@free.fr>
2002-02-24 16:34:51 +0000

    Version number changed

--- appli/server/src/server.h
+++ appli/server/src/server.h
@@ -32,7 +32,7 @@
 #define FS_ON_DOWNLOAD_SLEEP_RETRY 100
 #define FS_CHECK_EVERY_X_PING 10
 
-#define FFSS_SERVER_VERSION "1.0-pre75"
+#define FFSS_SERVER_VERSION "1.0-pre76"
 
 #ifdef DEBUG
 #define CONFIG_FILE_NAME "./Server.conf"
