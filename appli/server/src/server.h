b43ece48d0cd6b5850f5fe7d753977b6ccd7b1b2
kiki <dakikifr@free.fr>
2002-07-23 18:38:00 +0000

    Increased version

--- appli/server/src/server.h
+++ appli/server/src/server.h
@@ -46,7 +46,7 @@
 #define FS_ON_DOWNLOAD_SLEEP_RETRY 100
 #define FS_CHECK_EVERY_X_PING 10
 
-#define FFSS_SERVER_VERSION "1.0-pre83"
+#define FFSS_SERVER_VERSION "1.0-pre84"
 
 #ifdef DEBUG
 #define CONFIG_FILE_NAME "./Server.conf"
