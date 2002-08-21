4bdaa52395948cf12af2b72b13ee238bd00513e5
kiki <dakikifr@free.fr>
2002-08-21 20:34:58 +0000

    Added ffssdaemon

---
+++ appli/Install Setting/ffssdaemon/ffssdaemon.cpp
@@ -0,0 +1,239 @@
+// FFSSD_Service
+//
+// v1.0
+// KindMan 20/08/2002
+// v1.1
+// Christophe Calmejane 21/08/2002
+
+#include <ffss.h>
+
+#define FFSS_REGISTRY_PATH_PROCESSID FFSS_REGISTRY_PATH "Server\\ProcessId"
+#define FFSS_REGISTRY_PATH_SERVERDIR FFSS_REGISTRY_PATH "Server\\ServerDirectory"
+
+#define SLEEP_TIME 100
+#define MAX_WAIT 50
+
+#define ERR_CreateProcess 10000
+
+#include <windows.h>
+#include <string.h>
+#include <stdio.h>
+
+SERVICE_STATUS          FFSSDaemonStatus; 
+SERVICE_STATUS_HANDLE   FFSSDaemonStatusHandle; 
+
+VOID SvcDebugOut(LPSTR String, DWORD Status);
+VOID WINAPI FFSSDaemonStart (DWORD argc, char **argv); 
+VOID WINAPI FFSSDaemonCtrlHandler (DWORD opcode); 
+DWORD FFSSDaemonInitialization (DWORD argc, LPTSTR *argv,DWORD *specificError); 
+
+// main : Starts service
+
+void main()
+{ 
+    SERVICE_TABLE_ENTRY   DispatchTable[] = { 
+        { "FFSSDaemon", FFSSDaemonStart      }, 
+        { NULL,              NULL          } 
+    }; 
+
+    if (!StartServiceCtrlDispatcher( DispatchTable)) 
+    { 
+        SvcDebugOut(" [FFSSDaemon] StartServiceCtrlDispatcher error = %d\n", GetLastError()); 
+    } 
+} 
+ 
+VOID SvcDebugOut(LPSTR String, DWORD Status) 
+{ 
+    CHAR  Buffer[1024]; 
+    if (strlen(String) < 1000) 
+    { 
+        sprintf(Buffer, String, Status); 
+        OutputDebugStringA(Buffer); 
+    } 
+} 
+
+// Starts daemon
+
+VOID WINAPI FFSSDaemonStart (DWORD argc, char **argv) 
+{ 
+    DWORD status; 
+    DWORD specificError; 
+ 
+    FFSSDaemonStatus.dwServiceType        = SERVICE_WIN32; 
+    FFSSDaemonStatus.dwCurrentState       = SERVICE_START_PENDING; 
+    FFSSDaemonStatus.dwControlsAccepted   = SERVICE_ACCEPT_STOP; 
+    FFSSDaemonStatus.dwWin32ExitCode      = 0; 
+    FFSSDaemonStatus.dwServiceSpecificExitCode = 0; 
+    FFSSDaemonStatus.dwCheckPoint         = 0; 
+    FFSSDaemonStatus.dwWaitHint           = 0; 
+ 
+    FFSSDaemonStatusHandle = RegisterServiceCtrlHandler("FFSSDaemon", FFSSDaemonCtrlHandler); 
+ 
+    if (FFSSDaemonStatusHandle == (SERVICE_STATUS_HANDLE)0) 
+    { 
+        SvcDebugOut(" [FFSSDaemon] RegisterServiceCtrlHandler failed %d\n", GetLastError()); 
+        return; 
+    } 
+ 
+    // Initialization code goes here. 
+    status = FFSSDaemonInitialization(argc,argv, &specificError); 
+ 
+    // Handle error condition 
+    if (status != NO_ERROR) 
+    { 
+        FFSSDaemonStatus.dwCurrentState       = SERVICE_STOPPED; 
+        FFSSDaemonStatus.dwCheckPoint         = 0; 
+        FFSSDaemonStatus.dwWaitHint           = 0; 
+        FFSSDaemonStatus.dwWin32ExitCode      = status; 
+        FFSSDaemonStatus.dwServiceSpecificExitCode = specificError; 
+ 
+        SetServiceStatus (FFSSDaemonStatusHandle, &FFSSDaemonStatus); 
+        return; 
+    } 
+ 
+    // Initialization complete - report running status. 
+    FFSSDaemonStatus.dwCurrentState       = SERVICE_RUNNING; 
+    FFSSDaemonStatus.dwCheckPoint         = 0; 
+    FFSSDaemonStatus.dwWaitHint           = 0; 
+ 
+    if (!SetServiceStatus (FFSSDaemonStatusHandle, &FFSSDaemonStatus)) 
+    { 
+        status = GetLastError(); 
+        SvcDebugOut(" [FFSSDaemon] SetServiceStatus error %ld\n",status); 
+    } 
+ 
+    // This is where the service does its work. 
+    SvcDebugOut(" [FFSSDaemon] Returning the Main Thread \n",0); 
+ 
+    return; 
+} 
+ 
+// Stub initialization function. 
+DWORD FFSSDaemonInitialization(DWORD   argc, LPTSTR  *argv, DWORD *specificError) 
+{ 
+	STARTUPINFO startupinfo;
+	PROCESS_INFORMATION processinfo;
+	char buffer[1024];
+	char buffer2[1024];
+
+	startupinfo.cb = sizeof(startupinfo);
+	startupinfo.cbReserved2 = NULL;
+	startupinfo.lpReserved = NULL;
+	startupinfo.lpDesktop = NULL;
+	startupinfo.lpReserved2 = NULL;
+	startupinfo.lpTitle = NULL;	
+
+	// Get path to ffssd
+    SU_RB_GetStrValue(FFSS_REGISTRY_PATH_SERVERDIR,buffer,sizeof(buffer),"");
+    snprintf(buffer2,sizeof(buffer2),"%s\\ffssd.exe",buffer);
+
+	// Launch ffssd.exe
+
+	if (CreateProcess(NULL, buffer2, NULL, NULL, true, CREATE_NEW_CONSOLE | NORMAL_PRIORITY_CLASS,NULL, buffer, &startupinfo, &processinfo))
+	{
+		return NO_ERROR;
+	}
+	else
+	{
+		*specificError= GetLastError();
+		return ERROR_SERVICE_SPECIFIC_ERROR;
+	}
+
+	return NO_ERROR;
+} 
+
+void StopService()
+{
+	DWORD status; 
+
+	FFSSDaemonStatus.dwWin32ExitCode = 0; 
+	FFSSDaemonStatus.dwCurrentState  = SERVICE_STOPPED; 
+	FFSSDaemonStatus.dwCheckPoint    = 0; 
+	FFSSDaemonStatus.dwWaitHint      = 0; 
+ 
+	if (!SetServiceStatus (FFSSDaemonStatusHandle,&FFSSDaemonStatus))
+	{ 
+		status = GetLastError(); 
+		SvcDebugOut(" [FFSSDaemon] SetServiceStatus error %ld\n",status); 
+	} 
+ 
+	SvcDebugOut(" [FFSSDaemon] Leaving FFSSDaemon \n",0); 
+}
+
+VOID WINAPI FFSSDaemonCtrlHandler (DWORD opcode)
+{
+
+	DWORD status,res; 
+	DWORD pid=0;
+	DWORD spid=sizeof(pid);
+	HANDLE h;
+    int nb=0;
+
+    switch(opcode) 
+    { 
+        case SERVICE_CONTROL_PAUSE: 
+        // Do whatever it takes to pause here. 
+            FFSSDaemonStatus.dwCurrentState = SERVICE_PAUSED; 
+            break; 
+ 
+        case SERVICE_CONTROL_CONTINUE: 
+        // Do whatever it takes to continue here. 
+            FFSSDaemonStatus.dwCurrentState = SERVICE_RUNNING; 
+            break; 
+ 
+        case SERVICE_CONTROL_STOP: 
+        // Do whatever it takes to stop here. 
+
+			// Get FFSSD pid
+			// Try to kill it
+
+			pid = SU_RB_GetIntValue(FFSS_REGISTRY_PATH_PROCESSID,0);
+			if(!pid)
+			{
+				StopService();
+				return;
+			}
+			else
+			{
+
+				h = OpenProcess(PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION,false,pid);
+				if(h != NULL)
+				{
+					if (TerminateProcess(h, 0))
+					{
+                      do
+                      {
+                        if(GetExitCodeProcess(h,&res) == 0)
+                          break;
+                        if(res != STILL_ACTIVE)
+                        {
+						  StopService();
+						  return; 
+                        }
+                        Sleep(100);
+                        nb++;
+                      } while(nb < MAX_WAIT);
+					}
+				}
+
+				FFSSDaemonStatus.dwCurrentState = SERVICE_RUNNING;
+				break;
+			}
+	
+ 
+        case SERVICE_CONTROL_INTERROGATE: 
+        // Fall through to send current status. 
+            break; 
+ 
+        default: 
+            SvcDebugOut(" [FFSSDaemon] Unrecognized opcode %ld\n", opcode); 
+    } 
+ 
+    // Send current status. 
+    if (!SetServiceStatus (FFSSDaemonStatusHandle,  &FFSSDaemonStatus)) 
+    { 
+        status = GetLastError(); 
+        SvcDebugOut(" [FFSSDaemon] SetServiceStatus error %ld\n",status); 
+    } 
+    return; 
+} 
---
+++ appli/Install Setting/ffssdaemon/ffssdaemon.dsp
@@ -0,0 +1,102 @@
+# Microsoft Developer Studio Project File - Name="ffssdaemon" - Package Owner=<4>
+# Microsoft Developer Studio Generated Build File, Format Version 6.00
+# ** DO NOT EDIT **
+
+# TARGTYPE "Win32 (x86) Console Application" 0x0103
+
+CFG=ffssdaemon - Win32 Debug
+!MESSAGE This is not a valid makefile. To build this project using NMAKE,
+!MESSAGE use the Export Makefile command and run
+!MESSAGE 
+!MESSAGE NMAKE /f "ffssdaemon.mak".
+!MESSAGE 
+!MESSAGE You can specify a configuration when running NMAKE
+!MESSAGE by defining the macro CFG on the command line. For example:
+!MESSAGE 
+!MESSAGE NMAKE /f "ffssdaemon.mak" CFG="ffssdaemon - Win32 Debug"
+!MESSAGE 
+!MESSAGE Possible choices for configuration are:
+!MESSAGE 
+!MESSAGE "ffssdaemon - Win32 Release" (based on "Win32 (x86) Console Application")
+!MESSAGE "ffssdaemon - Win32 Debug" (based on "Win32 (x86) Console Application")
+!MESSAGE 
+
+# Begin Project
+# PROP AllowPerConfigDependencies 0
+# PROP Scc_ProjName ""
+# PROP Scc_LocalPath ""
+CPP=cl.exe
+RSC=rc.exe
+
+!IF  "$(CFG)" == "ffssdaemon - Win32 Release"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 0
+# PROP BASE Output_Dir "Release"
+# PROP BASE Intermediate_Dir "Release"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 0
+# PROP Output_Dir "Release"
+# PROP Intermediate_Dir "Release"
+# PROP Ignore_Export_Lib 0
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /c
+# ADD CPP /nologo /W3 /GX /O2 /I "../../../libffss-1.0.0/src" /I "../../../skyutils/src" /D "NDEBUG" /D "WIN32" /D "_CONSOLE" /D "_MBCS" /D "DISABLE_ZLIB" /D "DISABLE_BZLIB" /YX /FD /c
+# ADD BASE RSC /l 0x40c /d "NDEBUG"
+# ADD RSC /l 0x40c /d "NDEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib  kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /machine:I386
+# ADD LINK32 skyutils.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /machine:I386 /nodefaultlib:"libcmt" /libpath:"../../../skyutils/src/windows/skyutils/release"
+
+!ELSEIF  "$(CFG)" == "ffssdaemon - Win32 Debug"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 1
+# PROP BASE Output_Dir "Debug"
+# PROP BASE Intermediate_Dir "Debug"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 1
+# PROP Output_Dir "Debug"
+# PROP Intermediate_Dir "Debug"
+# PROP Ignore_Export_Lib 0
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_CONSOLE" /D "_MBCS" /YX /FD /GZ  /c
+# ADD CPP /nologo /W3 /Gm /GX /ZI /Od /I "../../../libffss-1.0.0/src" /I "../../../skyutils/src" /D "_DEBUG" /D "WIN32" /D "_CONSOLE" /D "_MBCS" /D "DISABLE_ZLIB" /D "DISABLE_BZLIB" /YX /FD /GZ  /c
+# ADD BASE RSC /l 0x40c /d "_DEBUG"
+# ADD RSC /l 0x40c /d "_DEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib  kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /debug /machine:I386 /pdbtype:sept
+# ADD LINK32 ws2_32.lib skyutils.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:console /debug /machine:I386 /nodefaultlib:"libcmt" /pdbtype:sept /libpath:"../../../skyutils/src/windows/skyutils/release"
+
+!ENDIF 
+
+# Begin Target
+
+# Name "ffssdaemon - Win32 Release"
+# Name "ffssdaemon - Win32 Debug"
+# Begin Group "Source Files"
+
+# PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
+# Begin Source File
+
+SOURCE=.\ffssdaemon.cpp
+# End Source File
+# End Group
+# Begin Group "Header Files"
+
+# PROP Default_Filter "h;hpp;hxx;hm;inl"
+# End Group
+# Begin Group "Resource Files"
+
+# PROP Default_Filter "ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe"
+# End Group
+# End Target
+# End Project
