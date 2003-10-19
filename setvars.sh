7c98d54c45a908f1eb154e13584e60b2fa3e606c
kiki <dakikifr@free.fr>
2003-10-19 14:18:18 +0000

    update

--- setvars.sh
+++ setvars.sh
@@ -2,5 +2,5 @@
 # If you are a ffss developper, change the anonymous with your login name (get one from kiki)
 # Then, run this script using ". ./setvars.sh"
 
-export CVSROOT=":pserver:anonymous:@orion.fleming.u-psud.fr:/home/cvs"
+export CVSROOT=":pserver:anonymous:@kiki.net0.nerim.net:/home/cvs"
 
